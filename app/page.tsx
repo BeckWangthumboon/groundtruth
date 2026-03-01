fdd227c06050c0050df5d9a702e046f278f66c29
Beck Wangthumboon <becktsm@gmail.com>
2026-02-28 20:07:19 -0600

    nuked

--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-import LandingHero from "@/components/groundtruth/LandingHero";
-
-export default function Home() {
-  return <LandingHero />;
-}
--- lib/groundtruth/config.ts
+++
@@ -1,9 +0,0 @@
-export const MAPBOX_TOKEN =
-  process.env.NEXT_PUBLIC_MAPBOX_TOKEN ?? process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN ?? "";
-
-export const MAP_STYLE_HERO = "mapbox://styles/mapbox/satellite-streets-v12";
-export const MAP_STYLE_GRID = "mapbox://styles/mapbox/dark-v11";
-
-export function hasMapboxToken(): boolean {
-  return MAPBOX_TOKEN.trim().length > 0;
-}
